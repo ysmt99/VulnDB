d2271b38d6c8948f54ef829c67e7f388f7441005
satomina <kotubutokotora@gmail.com>
2015-08-01 21:20:36 +0900

    SQLite版

---
+++ VulnDB/VulnDBSQLite/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using VulnDBSQLite.db;
+
+namespace VulnDBSQLite
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using(var ent = new VulnDBSQLiteEntities())
+            {
+                this.dataGridView1.DataSource = (from x in ent.SIDfm
+                                                orderby x.CVE番号
+                                                select x).ToList();
+            }
+        }
+    }
+}
