81ba17c22b279dc1dbb30c03aaae2cfa447cb5de
ysmt99 <ysmt99@gmail.com>
2015-08-04 20:02:08 +0900

    dbも追加

--- VulnDB/VulnDB/ErrorsByForm.cs
+++ VulnDB/VulnDB/ErrorsByForm.cs
@@ -10,111 +10,6 @@ using 入力規則 = VulnDB.Const.入力規則;
 
 namespace VulnDB
 {
-    ///// <summary>
-    ///// Validatorのエラーを格納
-    ///// 行（データ）⇒項目⇒ルールに対するエラー
-    ///// 1インスタンスは1行。その中に複数項目＋複数入力ルールがある
-    ///// </summary>
-    //class ErrorsByForm : Dictionary<KeyPair, List<string>>
-    //{
-    //    public void addError(CSV列 column, 入力規則 rule, string errorMessage) {
-    //        KeyPair column = new KeyPair(column, rule);
-    //        if (!this.ContainsKey(column))
-    //        {
-    //            this.Add(column, new List<string>() {errorMessage});
-    //        }
-    //        else
-    //        {
-    //            this[column].Add(errorMessage);
-    //        }
-    //    }
-    //    public string toString(CSV列 column)
-    //    {
-            
-    //        Enum.GetName(typeof(CSV列), this[Key.csv);
-    //    }
-    //}
-    //class ErrorsByColumn : Dictionary<入力規則, string>{
-    //    public static bool hasError(ErrorsByColumn obj)
-    //    {
-    //        // 値が入っていればtrue＝エラーである。
-    //        // 値が入っていなければfalse=エラーではない。
-    //        if (obj == null)
-    //        {
-    //            return false;
-    //        }
-    //        List<入力規則> rules = obj.Keys.ToList();
-    //        foreach (入力規則 rule in rules)
-    //        {
-    //            if (!String.IsNullOrEmpty(obj[rule]))
-    //            {
-    //                return true;
-    //            }
-    //        }
-    //        return true;
-    //    }
-    //};
-    //class ErrorsOfLine : Dictionary<CSV列, ErrorsByColumn> {
-    //    public void addError(CSV列 column, ErrorsByColumn rule)
-    //    {
-    //        // キーがない場合、および値のListがnullの場合はリストを作る
-    //        if (!ContainsKey(column) || this[column] == null)
-    //        {
-    //            List<ErrorsByColumn> list = new List<ErrorsByColumn>();
-    //            this[column] = list;
-    //        }
-    //        this[column].Add(rule);
-    //    }
-    //    public static bool hasError(ErrorsOfLine obj)
-    //    {
-    //        List<CSV列> columns = obj.Keys.ToList();
-    //        List<ErrorsByColumn> rulesObjects;
-    //        foreach (CSV列 c in columns)
-    //        {
-    //            rulesObjects = obj[c];
-    //            foreach (ErrorsByColumn ruleObj in rulesObjects)
-    //            {
-    //                if (ErrorsByColumn.hasError(ruleObj))
-    //                {
-    //                    return true;
-    //                }
-    //            }
-    //        }
-    //        return false;
-    //    }
-    //};
-
-    //class ErrorsByForm : Dictionary<int, ErrorsOfLine>
-    //{
-    //    public void addError(int line, ErrorsOfLine obj)
-    //    {
-    //        // キーがない場合、および値のListがnullの場合はリストを作る
-    //        if (!ContainsKey(line) || this[line] == null)
-    //        {
-    //            List<ErrorsOfLine> list = new List<ErrorsOfLine>();
-    //            this[line] = list;
-    //        }
-    //        this[line].Add(obj);
-    //    }
-    //    public static bool hasError(ErrorsByForm obj)
-    //    {
-    //        List<int> lines = obj.Keys.ToList();
-    //        List<ErrorsOfLine> columnsObjects;
-    //        foreach (int i in lines)
-    //        {
-    //            columnsObjects = obj[i];
-    //            foreach (ErrorsOfLine c in columnsObjects)
-    //            {
-    //                if (ErrorsOfLine.hasError(c))
-    //                {
-    //                    return true;
-    //                }
-    //            }
-    //        }
-    //        return false;
-    //    }
-    //}
-
     // tree構造クラス
     // V⇒クラスのkey
     // T⇒ディクショナリのkey
@@ -206,6 +101,11 @@ namespace VulnDB
         }
         public List<string> getErrorMessages()
         {
+            if (!hasError()) // エラーがなければ空のリスト
+            {
+                return new List<string>();
+            }
+            // エラーがあればエラーメッセージをリストにして返す
             return new List<string>() {this.s};
         }
     }
--- VulnDB/VulnDB/SIDfmValidator.cs
+++ VulnDB/VulnDB/SIDfmValidator.cs
@@ -10,58 +10,9 @@ namespace VulnDB
 {
     class ValidateParams {
 
-        //public CSV列 column;
-        //public List<入力規則> rules;
-
         public static Dictionary<CSV列, List<入力規則>> param;
-        //public ValidateParams() { }
-        //public ValidateParams(CSV列 column, List<入力規則> rules)
-        //{
-        //    this.column = column;
-        //    this.rules = rules;
-        //}
-
-        //public static List<ValidateParams> param;
         static ValidateParams() {
-            //param = new List<ValidateParams>();
-            
-            //param.Add(new ValidateParams(CSV列.SIDfmId,new List<入力規則>() { 入力規則.必須, 入力規則.整数 }));
-            //param.Add(new ValidateParams(CSV列.タイトル,new List<入力規則>() { 入力規則.必須 }));
-            //param.Add(new ValidateParams(CSV列.CVE番号,new List<入力規則>() { 入力規則.必須 }));
-            //param.Add(new ValidateParams(CSV列.CVSS基本値,new List<入力規則>() { 入力規則.数値_小数点あり }));
-            //param.Add(new ValidateParams(CSV列.攻撃元_ローカル,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃元_隣接,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃元_ネットワーク,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃成立条件_難しい,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃成立条件_やや難,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃成立条件_簡単,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃前の認証_複数,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃前の認証_単一,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃前の認証_不要,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.情報漏えい_影響無し,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.情報漏えい_部分的,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.情報漏えい_全面的,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.情報改ざん_影響無し,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.情報改ざん_部分的,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.情報改ざん_全面的,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.業務停止_影響無し,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.業務停止_部分的,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.業務停止_全面的,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.攻撃コードの有無,new List<入力規則>() { 入力規則.スイッチ_0か1 }));
-            //param.Add(new ValidateParams(CSV列.情報登録日,new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム1, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム2, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム3, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム4, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム5, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム6, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム7, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム8, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム9, new List<入力規則>() { 入力規則.日付 }));
-            //param.Add(new ValidateParams(CSV列.アイテム10, new List<入力規則>() { 入力規則.日付 }));
-
             param = new Dictionary<CSV列, List<入力規則>>();
-
             param.Add(CSV列.SIDfmId, new List<入力規則>() { 入力規則.必須, 入力規則.整数 });
             param.Add(CSV列.タイトル, new List<入力規則>() { 入力規則.必須 });
             param.Add(CSV列.CVE番号, new List<入力規則>() { 入力規則.必須 });
@@ -98,98 +49,6 @@ namespace VulnDB
             param.Add(CSV列.アイテム10, new List<入力規則>() { 入力規則.日付 });
         }
     }
-    //class SIDfmValidator
-    //{
-    //    //public CSV列 column;
-    //    //public List<入力規則> rules;
-
-    //    //public SIDfmValidator() {}
-    //    //public SIDfmValidator(CSV列 column, List<入力規則> rules)
-    //    //{
-    //    //    this.column = column;
-    //    //    this.rules = rules;
-    //    //}
-
-    //    //public ErrorOfForm validateLine(int lineNo)
-    //    //{
-    //    //    ErrorOfForm errorOfLine = new ErrorOfForm();
-    //    //    ErrorOfColumn errorOfColumn;
-    //    //    SIDfmValidator validator;
-    //    //    foreach (CSV列 c in Enum.GetValues(typeof(CSV列)))
-    //    //    {
-    //    //        validator = getValidator(c);
-    //    //        if (validator != null)
-    //    //        {
-    //    //            errorOfColumn = validator.validateByColumn(values[c]);
-    //    //            errorOfLine.addError(validator.column, errorOfColumn);
-    //    //        }
-    //    //    }
-    //    //    return errorOfLine;
-    //    //}
-
-    //    ///// <summary>
-    //    ///// 項目ごとの入力チェック
-    //    ///// </summary>
-    //    ///// <returns></returns>
-    //    //public ErrorOfColumn validateByColumn(string value)
-    //    //{
-    //    //    ErrorOfColumn error = new ErrorOfColumn();
-    //    //    foreach (入力規則 rule in rules)
-    //    //    {
-    //    //        error.addError(rule,validateByRule(column,rule,value));
-    //    //    }
-    //    //    return error;
-    //    //}
-    //    ///// <summary>
-    //    /// ルールごとの入力チェック
-    //    /// </summary>
-    //    /// <returns></returns>
-    //    public string validateByRule(CSV列 key, 入力規則 rule, string value)
-    //    {
-    //        string errorMessage = "";
-    //        string name = Enum.GetName(typeof(CSV列), key);
-
-    //        // 必須の場合は無条件に値を見る
-    //        // 必須以外の場合、値が入っていたら書式チェックを行う
-    //        switch (rule)
-    //        {
-    //            case 入力規則.必須:
-    //                if (String.IsNullOrEmpty(value))
-    //                {
-    //                    errorMessage = String.Format("{0}が未入力です。", name);
-    //                }
-    //                break;
-    //            case 入力規則.整数:
-    //                int i;
-    //                if (!String.IsNullOrEmpty(value) && !Int32.TryParse(value, out i))
-    //                {
-    //                    errorMessage = String.Format("{0}が整数ではありません。", name);
-    //                }
-    //                break;
-    //            case 入力規則.数値_小数点あり:
-    //                decimal d;
-    //                if (!String.IsNullOrEmpty(value) && !Decimal.TryParse(value, out d))
-    //                {
-    //                    errorMessage = String.Format("{0}が数値ではありません。", name);
-    //                }
-    //                break;
-    //            case 入力規則.日付:
-    //                DateTime dt;
-    //                if (!String.IsNullOrEmpty(value) && !DateTime.TryParse(value, out dt))
-    //                {
-    //                    errorMessage = String.Format("{0}の日付形式が正しくありません。", name);
-    //                }
-    //                break;
-    //            case 入力規則.スイッチ_0か1:
-    //                if (!String.IsNullOrEmpty(value) && !"0".Equals(value) && !"1".Equals(value))
-    //                {
-    //                    errorMessage = String.Format("{0}は0か1を指定してください。", name);
-    //                }
-    //                break;
-    //        }
-    //        return errorMessage;
-    //    }
-    //}
     public class SIDfmLineValidator
     {
         public int lineNo { set; get; }
